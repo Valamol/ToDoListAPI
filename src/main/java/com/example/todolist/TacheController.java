ee1dd14bf9fea6b5b3f6d3c65cd710c5a9ee0aa6
Valamol <delamauryandre@gmail.com>
2023-04-04 13:31:23 +0200

    API

--- src/main/java/com/example/todolist/TacheController.java
+++ src/main/java/com/example/todolist/TacheController.java
@@ -24,7 +24,6 @@ public class TacheController {
     }
     @PostMapping
     public Taches createTache(@RequestBody Taches taches) {
-        System.out.println("add");
         return tacheRepository.save(taches);
     }
 }
